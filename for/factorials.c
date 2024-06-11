c2378f357703b32f053b28c4853230b56e9c2598
M. Hanzala AKTU <mhnzlakt56@gmail.com>
2024-06-11 22:23:26 +0300

    düzeltme

--- for/factorials.c
+++ for/factorials.c
@@ -3,15 +3,15 @@
 int main()
 {
     int max;
-    printf("Enter the maximum integer for factorial calculation:\n ");
+    printf("Enter the maximum integer for factorial calculation: ");
     scanf("%d", &max);
 
-    int factorial = 1;
+    long long factorial = 1;
     for (int i = 1; i <= max; i++) 
     {
         factorial *= i;
     }
-    printf("Factorials %d is : %d\n",max, factorial);
+    printf("\n Factorials %d is : %llu\n",max, factorial);
     
 
     return 0;

e6642544e8f58744c4c7e67e706e6661317da8c7
M. Hanzala AKTU <mhnzlakt56@gmail.com>
2024-08-20 10:30:54 +0300

    ekleme

--- for/triangle-3.c
+++ for/triangle-3.c
@@ -1,22 +1,27 @@
 /*
  *****
- ****
- ***
- **
- *
-*/
+  ****
+   ***
+    **
+     *
+ */
 
 #include <stdio.h> 
 
 int main()
 {
-    int t,i,j;
+    int t,i,j,k;
 
     printf("Enter trinagle: ");
     scanf("%d",&t);
     
     for (i = t; i > 0; i--)
     {
+        for ( k = 0; k < t-i; k++)
+        {
+            printf(" ");
+        }
+        
         for (j = 0; j < i; j++)
         {
             printf("*");
@@ -25,5 +30,5 @@ int main()
         printf("\n");    
 
     }
-
+    
 }
